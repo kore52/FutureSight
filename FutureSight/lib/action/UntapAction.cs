e47269a666da5e9834b6a0aeb1123366297d5f35
kore52 <lm312v21@gmail.com>
2016-04-06 00:32:13 +0900

    TEST

---
+++ FutureSight/lib/action/UntapAction.cs
@@ -0,0 +1,31 @@
+﻿namespace FutureSight.lib
+{
+    public class UntapAction : MTGAction
+    {
+        private MTGPermanent permanent;
+        private bool isTapped;
+
+        public UntapAction(MTGPermanent permanent)
+        {
+            this.permanent = permanent;
+        }
+
+        // アクションを行う
+        public override void DoAction(GameState game)
+        {
+            isTapped = permanent.IsTapped();
+            if (isTapped)
+            {
+                permanent.RemoveState(MTGPermanentState.Tapped);
+                game.SetStateCheckRequired();
+            }
+        }
+
+        // アクションを戻す
+        public override void UndoAction(GameState game)
+        {
+            if (isTapped)
+                permanent.AddState(MTGPermanentState.Tapped);
+        }
+    }
+}
